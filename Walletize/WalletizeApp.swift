66aaedfef5588e9032c02b543bb9f93ab9e46fc4
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-02-01 13:04:50 +0700

    Initial Commit

---
+++ Walletize/WalletizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletizeApp.swift
+//  Walletize
+//
+//  Created by Yap Justin on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
